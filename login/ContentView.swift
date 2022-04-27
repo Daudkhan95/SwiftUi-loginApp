4005fe6e067eec08f7fb6714b7fbe883e23da1cf
Lazy Programmer <daudkhan058058@gmail.com>
2022-04-28 06:35:36 +0800

    Initial Commit

---
+++ login/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  login
+//
+//  Created by Daud Khan on 4/28/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ login/loginApp.swift
@@ -0,0 +1,17 @@
+//
+//  loginApp.swift
+//  login
+//
+//  Created by Daud Khan on 4/28/22.
+//
+
+import SwiftUI
+
+@main
+struct loginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
